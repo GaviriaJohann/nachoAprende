782deae3f88440ee7bac281d0ae8834194d68532
JohannGaviria <johannmgr@gmail.com>
2024-03-06 22:00:15 -0500

    se avanza en la funcion de busqueda

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNachoAprende));
             this.wmpMusica = new AxWMPLib.AxWindowsMediaPlayer();
             this.wbNavegador = new AxSHDocVw.AxWebBrowser();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblSearch = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.wmpMusica)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.wbNavegador)).BeginInit();
             this.SuspendLayout();
@@ -52,11 +55,40 @@
             this.wbNavegador.Size = new System.Drawing.Size(382, 346);
             this.wbNavegador.TabIndex = 0;
             // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(26, 411);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(204, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(257, 411);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(26, 392);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(147, 13);
+            this.lblSearch.TabIndex = 4;
+            this.lblSearch.Text = "Ingrese el contenido a buscar";
+            // 
             // frmNachoAprende
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(748, 450);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.wmpMusica);
             this.Controls.Add(this.wbNavegador);
             this.Name = "frmNachoAprende";
@@ -65,6 +97,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.wmpMusica)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.wbNavegador)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +105,9 @@
 
         private AxSHDocVw.AxWebBrowser wbNavegador;
         private AxWMPLib.AxWindowsMediaPlayer wmpMusica;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblSearch;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,10 @@
 ﻿using AxWMPLib;
+using SHDocVw;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,31 @@ namespace NachoAprende
         {
             string videoPath = @"C:\Users\johan\source\repos\NachoAprende\bin\Debug\video.mp4";
             wmpMusica.URL = videoPath;
+
+            
+        }
+
+        private void RealizarBusquedaEnLinea(string busqueda)
+        {
+            string urlBusqueda = "https://www.google.com/search?q=" + Uri.EscapeUriString(busqueda);
+
+            wbNavegador.Navigate(urlBusqueda);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string search = txtSearch.Text.Trim();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                search = search.Replace(" ", "+");
+
+                RealizarBusquedaEnLinea(search);
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese un término de búsqueda.");
+            }
         }
     }
 }
